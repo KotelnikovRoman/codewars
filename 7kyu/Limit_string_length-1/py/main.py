72a556c7d7c745a3a0c37603dc38dbbdb56399b3
Roman Kotelnikov <kotelnikovrm@gmail.com>
2021-02-14 08:08:55 +0800

    update code python in limit string length-1 (7kyu)

--- 7kyu/Limit_string_length-1/py/main.py
+++ 7kyu/Limit_string_length-1/py/main.py
@@ -1,4 +1,26 @@
+# my code
 def solution(st, limit):
     if len(st) <= limit:
         return st
     return st[0:limit] + "..."
+
+# codewars
+
+
+def solution2(st, limit):
+    return st[:limit] + '...' if limit < len(st) else st
+
+
+def solution3(st, limit):
+    return f'{st[:limit]}{"..." * (len(st) > limit)}'
+
+
+def solution4(st, limit):
+    return st if len(st) <= limit else "".join(list(st)[:limit]) + "..."
+
+
+def solution5(st, limit):
+    return st[:limit] + '...' * (limit < len(st))
+
+
+print(solution4("my code prosto super", 7))
