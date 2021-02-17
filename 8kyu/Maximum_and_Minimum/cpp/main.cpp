f87648df2ff37a1b9b1b794cbcade1c11104811a
Roman Kotelnikov <kotelnikovrm@gmail.com>
2021-02-17 19:57:38 +0800

    add code cpp in Maximum and Minimum (8kyu)

--- 8kyu/Maximum_and_Minimum/cpp/main.cpp
+++ 8kyu/Maximum_and_Minimum/cpp/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
+//for min2, max2
+#include <algorithm>
 using namespace std;
 
 int min(vector<int> list) {
 	int min_n = list[0];
-	
+
 	for (int i = 0; i < size(list); i++) {
 		if (min_n > list[i]) {
 			min_n = list[i];
@@ -26,7 +28,27 @@ int max(vector<int> list) {
 	return max_n;
 }
 
+//codewars
+int min2(vector<int> list) {
+	return *(min_element(list.begin(), list.end()));
+}
+
+int max2(vector<int> list) {
+	return *(max_element(list.begin(), list.end()));
+}
+
+int min3(vector<int> list) {
+	return *min_element(begin(list), end(list));
+}
+
+int max3(vector<int> list) {
+	return *max_element(begin(list), end(list));
+}
+
 int main() {
 	vector<int> arr = { 2, 5, 0, 7, -1, 10 };
-	cout << max(arr) << endl;
+	cout << min3(arr) << endl;
+	cout << max3(arr) << endl;
+
+	return 0;
 }
