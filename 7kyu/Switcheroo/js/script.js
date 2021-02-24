339311399ac5dc678cde7baba27d3adb5e97a0ee
Roman Kotelnikov <kotelnikovrm@gmail.com>
2021-02-24 14:27:30 +0800

    update js in switcheroo

--- 7kyu/Switcheroo/js/script.js
+++ 7kyu/Switcheroo/js/script.js
@@ -17,3 +17,41 @@ function switcheroo(x) {
 
     return txt;
 }
+
+//codewars
+switcheroo2 = x => x.replace(/[ab]/g, x => x == "a" ? "b" : "a");
+
+function switcheroo3(x) {
+    return x.split('').map(function (e) {
+        if (e == 'b') return 'a';
+        if (e == 'a') return 'b';
+        if (e == 'c') return 'c';
+    }).join('');
+}
+
+function switcheroo4(x) {
+    return x.replace(
+        /[ab]/g,
+        function (e) {
+            return e == 'a' ? 'b' : 'a';
+        }
+    );
+}
+
+function switcheroo5(x) {
+    return [...x].map(v => v == 'a' ? 'b' : v == 'b' ? 'a' : v).join('');
+}
+
+switcheroo6 = x => x.replace(/./g, x => 'abc'['bac'.indexOf(x)]);
+
+function switcheroo7(x) {
+    return x.split('').map(item => {
+        switch (item) {
+            case 'a': return 'b';
+            case 'b': return 'a';
+            default: return item;
+        }
+    }).join('');
+}
+
+document.write(switcheroo7("aabcab"));
